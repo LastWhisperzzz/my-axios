0e1539d4e4ee6e1e37c24b4648b44d30e3595826
Last Whisper <xxzworkmail@gmail.com>
2021-03-30 14:03:12 +0800

    feat: util函数

--- axios.js
+++ axios.js
@@ -1,16 +1,55 @@
-class Axios{
+class Axios {
   constructor(defaultConfig) {
     this.defaultConfig = defaultConfig;
+    this.defaultConfig = {};
+    this.test = "test";
   }
   fetch() {
-    return Promise.resolve(1)
+    return Promise.resolve(1);
   }
 }
 
 function createInstance() {
-  let context = new Axios();
-  let instance = context.fetch
-  return instance
+  let utils = {
+    /**
+     * 把相关属性添加到实例上
+     * @param instance
+     * @param attributes
+     * @param context
+     */
+    extends(instance, attributes, context) {
+      for (let key in attributes) {
+        if (attributes.hasOwnProperty(key)) {
+          if (typeof attributes[key] === "function") {
+            instance[key] = attributes[key].bind(context);
+          } else {
+            instance[key] = attributes[key];
+          }
+        }
+      }
+    },
+  };
+
+  //扩展对象
+  methodsArrList = ["post", "put", "get", "head", "options", "delete"];
+  methodsArrList.forEach((method) => {
+    Axios.prototype[method] = function () {
+      return this.fetch();
+    };
+  });
+
+  console.dir(Axios);
 }
 
-let axios = createInstance()
+/**
+ * 创建实例
+ * @param defaultConfig
+ * @returns {function(): Promise<number>}
+ */
+function createInstance(defaultConfig) {
+  let context = new Axios();
+  let instance = context.fetch;
+  utils.extends(instance, Axios.prototype, context);
+  utils.extends(instance, context);
+  return instance;
+}
